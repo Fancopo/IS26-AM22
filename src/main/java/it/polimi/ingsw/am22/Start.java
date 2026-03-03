70ed52515e8c122bd748b55fa267d09aaada8885
Jiaji Huang <jiajihuang@Jiajis-MacBook-Air.local>
2026-03-03 14:43:05 +0100

    Initial version

---
+++ src/main/java/it/polimi/ingsw/am22/Start.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.am22;
+
+import javafx.application.Application;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Ciao"+ args[0]);
+    }
+}
