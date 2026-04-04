e609d7e88e3013d23e100f2a50c39084e9e8ba39
Tao Chen <tao.chen@mail.polimi.it>
2026-04-04 15:38:50 +0200

    turn into final some variables,

--- src/main/java/it/polimi/ingsw/am22/GameState.java
+++ src/main/java/it/polimi/ingsw/am22/GameState.java
@@ -11,7 +11,7 @@ public interface GameState {
     default void placeTotemOnOffer(Game game, Player player, OfferTile tile) {
         throw new IllegalStateException("Azione non permessa in: " + getPhaseName());
     }
-    default void pickCards(Game game, Player player, List<Card> selectedCards) {
+    default void pickCards2(Game game, Player player, List<Card> selectedCards) {
         throw new IllegalStateException("Azione non permessa in: " + getPhaseName());
     }
     default void pickBonusCard(Game game, Player player, Card bonusCard) {
@@ -27,4 +27,6 @@ public interface GameState {
         throw new IllegalStateException("Azione non permessa in: " + getPhaseName());
     }
     String getPhaseName();
+
+    void pickCards2(Game game, Player player, List<Card> selectedCards);
 }
