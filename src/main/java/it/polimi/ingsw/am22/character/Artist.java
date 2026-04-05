bdbaeb040b5a73c3a0da02671d9219cace3486e1
CHRISTIAN <christian.fang@mail.polimi.it>
2026-04-05 22:29:20 +0200

    last commit 2

---
+++ src/main/java/it/polimi/ingsw/am22/character/Artist.java
@@ -0,0 +1,13 @@
+package il.polimi.ingsw.am22.character;
+
+public class Artist extends TribeCharacter implements CharacterEffect {
+
+    public Artist(char id, String type, int era, int minPlayers, String characterType){
+        super(id, type, era, minPlayers, "Artist");
+    }
+
+
+    @Override
+    public void applyImmediateEffect(Player player, Tribe tribe) {
+    }
+}
